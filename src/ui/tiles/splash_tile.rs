6813c625216e6285567a13ca509c526b2525bf05
Kenny Levinsen <kl@kl.wtf>
2025-03-23 00:38:54 +0100

    WIP splash calendar, battery

--- src/ui/tiles/splash_tile.rs
+++ src/ui/tiles/splash_tile.rs
@@ -1,7 +1,7 @@
-use gtk4::{Builder, Label, ListBoxRow, glib};
-
 use super::Tile;
 use crate::launcher::Launcher;
+use gio::{prelude::*, BusType, Cancellable, DBusProxy, DBusProxyFlags};
+use gtk4::{glib, prelude::WidgetExt, Builder, Label, ListBoxRow, ProgressBar};
 
 fn update_time(time_label: &Label, date_top: &Label, date_bottom: &Label) {
     let now = chrono::Local::now();
@@ -20,6 +20,7 @@ impl Tile {
         let time_label: Label = builder.object("time-bar").unwrap();
         let date_top: Label = builder.object("date-top").unwrap();
         let date_bottom: Label = builder.object("date-bottom").unwrap();
+        let bat_value: ProgressBar = builder.object("battery-value").unwrap();
 
         update_time(&time_label, &date_top, &date_bottom);
 
@@ -28,12 +29,47 @@ impl Tile {
             glib::ControlFlow::Continue
         });
 
-        // builder.category.set_text(name);
-        // builder.icon.set_icon_name(Some(icon));
-        // builder.title.set_text(keyword);
-        //builder.add_default_attrs(Some(&launcher.method), None, Some(keyword), None, None);
-        
+        let cancel = Cancellable::new();
+        DBusProxy::for_bus(
+            BusType::System,
+            DBusProxyFlags::NONE,
+            None,
+            "org.freedesktop.UPower",
+            "/org/freedesktop/UPower/devices/DisplayDevice",
+            "org.freedesktop.UPower.Device",
+            Some(&cancel),
+            move |res| {
+                if let Ok(proxy) = res {
+                    if let Some(state) = proxy.cached_property("State") {
+                        let value: u32 = state.get().unwrap();
+                        match value {
+                            1 => bat_value.add_css_class("battery-charging"),
+                            2 => bat_value.add_css_class("battery-discharging"),
+                            3 => bat_value.add_css_class("battery-empty"),
+                            4 => bat_value.add_css_class("battery-fully-charged"),
+                            5 => bat_value.add_css_class("battery-pending-charge"),
+                            6 => bat_value.add_css_class("battery-pending-discharge"),
+                            _ => (),
+                        };
+                    }
+                    if let Some(cap) = proxy.cached_property("Percentage") {
+                        let value: f64 = cap.get().unwrap();
+                        bat_value.set_fraction(value / 100.0);
+                    }
+                    if let Some(cap) = proxy.cached_property("WarningLevel") {
+                        let value: u32 = cap.get().unwrap();
+                        match value {
+                            3 => bat_value.add_css_class("battery-low"),
+                            4 => bat_value.add_css_class("battery-critical"),
+                            5 => bat_value.add_css_class("battery-action"),
+                            _ => (),
+                        }
+                    }
+                }
+            },
+        );
+
         let object: ListBoxRow = builder.object("holder").unwrap_or_default();
-        return (index + 1, vec![object]);
+        return (index + 2, vec![object]);
     }
 }
